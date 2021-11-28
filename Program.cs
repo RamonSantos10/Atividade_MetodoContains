0d7d1b28d0cb4471f9688bd9d6eac518c694b22e
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2021-11-28 14:28:36 -0300

    Metodo_Contains

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace MetodoContains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo_Contains1();
+            WriteLine("-----------------");
+            Metodo_Contains2();
+            
+        }
+
+        private static void Metodo_Contains1()
+        {
+            string nomes = "Ramon, Pedro, Bruno, Antonio, Victor";
+            bool listaNomes = nomes.Contains('y');
+
+            WriteLine(listaNomes);
+        }
+
+        private static void Metodo_Contains2()
+        {
+            string nomes = "Ramon, Pedro, Bruno, Antonio, Victor";
+            bool listaNomes = nomes.Contains(',');
+
+            WriteLine(listaNomes);
+        }
+    }
+}
